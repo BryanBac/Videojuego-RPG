433b0ebc462c10209aea9d47183e93acee9165d6
Kathya González <49594812+KathyaGonzalez@users.noreply.github.com>
2022-06-14 19:53:51 -0600

    Personajes inicial

---
+++ models/personaje/sanador.py
@@ -0,0 +1,28 @@
+from models.personaje.personaje import Personaje
+
+
+class Sanador(Personaje):
+    def __init__(self, hp, mp, st, pos_x, pos_y):
+        self.__hp: int = hp
+        self.__mp: int = mp
+        self.__st: int = st
+        self.__pos_x: int = pos_x
+        self.__pos_y: int = pos_y
+
+    def izquierda(self):
+        pass
+
+    def derecha(self):
+        pass
+
+    def saltar(self):
+        pass
+
+    def golpear(self) -> float:
+        pass
+
+    def super_ataque(self) -> float:
+        pass
+
+    def revivir(self):
+        pass
