2bd947cf52571c4e159b130086e04fb323b671b7
BryanBac <49624724+BryanBac@users.noreply.github.com>
2022-06-17 11:04:07 -0600

    Movimiento-Izq-Der
    Se implementaron movimientos con assets provicionales para izquierda y derecha de dos personajes

--- main.py
+++ main.py
@@ -1,25 +1,104 @@
 import pygame
 from sys import exit
 
-
 pygame.init()
 ancho = 800
 alto = 400
 screen = pygame.display.set_mode((ancho, alto))
 pygame.display.set_caption("Inmersion Quest")
 clock = pygame.time.Clock()
+#  texto
 test_font = pygame.font.Font(None, 20)
-text_surface1 = test_font.render("Nueva Partida", False, "White")
-text_surface2 = test_font.render("Cargar Partida", False, "White")
+text_surface1 = test_font.render("PvP", False, "White")
+text_surface2 = test_font.render("PC", False, "White")
 text_surface3 = test_font.render("Salir", False, "White")
 
+#  imagenes -- Estas son de prueba para ver como funciona esto
+fondo_surface = pygame.image.load("assets/fondo.jpg").convert()
+
+piso_surface = pygame.image.load("assets/ch.png").convert()
+piso_surface = pygame.transform.scale(piso_surface, (ancho, 50))
+
+cuatro_brazos = pygame.image.load("assets/cuatrobrazos.png").convert_alpha()
+cuatro_brazos = pygame.transform.scale(cuatro_brazos, (150, 100))
+rec_cuatro_brazos = cuatro_brazos.get_rect(topright=(150, 250))
+
+kevin11 = pygame.image.load("assets/kevin11.png").convert_alpha()
+ancho_villano = 150
+kevin11 = pygame.transform.scale(kevin11, (ancho_villano, 180))
+rec_kevin11 = kevin11.get_rect(topright=(656-ancho_villano, 250))
+
+#  variables
 eleccion = 1
 pantalla = 0
+
+
+def pantalla_inicio(text_surface1, text_surface2, text_surface3):
+    if eleccion == 1:
+        text_surface2 = test_font.render("PC", False, "White")
+        text_surface1 = test_font.render("PvP", False, "Cyan")
+    elif eleccion == 2:
+        text_surface1 = test_font.render("PvP", False, "White")
+        text_surface3 = test_font.render("Salir", False, "White")
+        text_surface2 = test_font.render("PC", False, "Cyan")
+    elif eleccion == 3:
+        text_surface2 = test_font.render("PC", False, "White")
+        text_surface3 = test_font.render("Salir", False, "Cyan")
+    screen.blit(text_surface1, (ancho / 3, alto / 3))
+    screen.blit(text_surface2, (ancho / 3, alto / 3 + 50))
+    screen.blit(text_surface3, (ancho / 3, alto / 3 + 100))
+
+
+def pantalla_pvp(fondo, personaje, rect_personaje, villano, rec_villano):
+    screen.blit(fondo, (0, 0))
+    screen.blit(piso_surface, (0, 350))
+    screen.blit(personaje, rect_personaje)
+    screen.blit(villano, rec_villano)
+
+
+presion_izq: bool = False
+presion_der: bool = False
+
+presion_A: bool = False
+presion_D: bool = False
 while True:
+    #  Movimiento
+    if presion_A:
+        rec_cuatro_brazos.left -= 4
+        if rec_cuatro_brazos.left == -4:
+            rec_cuatro_brazos.left = 0
+    if presion_D:
+        rec_cuatro_brazos.left += 4
+        if rec_cuatro_brazos.left == 656:
+            rec_cuatro_brazos.left = 652
+    if presion_izq:
+        rec_kevin11.left -= 4
+        if rec_kevin11.left == -4:
+            rec_kevin11.left = 0
+    if presion_der:
+        rec_kevin11.left += 4
+        if rec_kevin11.left == 656:
+            rec_kevin11.left = 652
+    #  presión de teclas
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        #  teclas levantadas
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                if pantalla == 1:
+                    presion_izq = False
+            if event.key == pygame.K_RIGHT:
+                if pantalla == 1:
+                    presion_der = False
+            if event.key == pygame.K_a:
+                if pantalla == 1:
+                    presion_A = False
+            if event.key == pygame.K_d:
+                if pantalla == 1:
+                    presion_D = False
+        #  teclas oprimidas
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 if pantalla == 0:
@@ -31,6 +110,18 @@ while True:
                     eleccion += 1
                     if eleccion == 4:
                         eleccion = 3
+            if event.key == pygame.K_LEFT:
+                if pantalla == 1:
+                    presion_izq = True
+            if event.key == pygame.K_RIGHT:
+                if pantalla == 1:
+                    presion_der = True
+            if event.key == pygame.K_a:
+                if pantalla == 1:
+                    presion_A = True
+            if event.key == pygame.K_d:
+                if pantalla == 1:
+                    presion_D = True
             if event.key == pygame.K_RETURN:
                 if pantalla == 0:
                     if eleccion == 1:
@@ -45,18 +136,8 @@ while True:
                         print("Saliendo...")
                         exit()
     if pantalla == 0:
-        if eleccion == 1:
-            text_surface2 = test_font.render("Cargar Partida", False, "White")
-            text_surface1 = test_font.render("Nueva Partida", False, "Cyan")
-        elif eleccion == 2:
-            text_surface1 = test_font.render("Nueva Partida", False, "White")
-            text_surface3 = test_font.render("Salir", False, "White")
-            text_surface2 = test_font.render("Cargar Partida", False, "Cyan")
-        elif eleccion == 3:
-            text_surface2 = test_font.render("Cargar Partida", False, "White")
-            text_surface3 = test_font.render("Salir", False, "Cyan")
-        screen.blit(text_surface1, (ancho/3, alto/3))
-        screen.blit(text_surface2, (ancho/3, alto/3 + 50))
-        screen.blit(text_surface3, (ancho/3, alto/3 + 100))
+        pantalla_inicio(text_surface1, text_surface2, text_surface3)
+    elif pantalla == 1:
+        pantalla_pvp(fondo_surface, cuatro_brazos, rec_cuatro_brazos, kevin11, rec_kevin11)
     pygame.display.update()
     clock.tick(60)
