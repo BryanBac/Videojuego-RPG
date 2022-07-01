bd8e9e183614dd901bb303cfda91625b2af7829c
Laura <belen___250@hotmail.com>
2022-06-30 21:24:49 -0600

    Implementación de animaciones y saltos
    - Se implementó la función de saltos
    - Se agregaron los assets terminados y se utilizaron para animar los movimientos de los personajes

--- models/personaje/personaje.py
+++ models/personaje/personaje.py
@@ -13,13 +13,28 @@ class Personaje(metaclass=ABCMeta):
         self.__h: int = h
         self.colision_actual = False
         self.colision_anterior = False
-        self.__items: Dict[str, int] = {('Item 1', 2), ('Item 2', 3), ('Item 3', 4)}
+        self.__items: Dict[str, int] = {("Item 1", 2), ("Item 2", 3), ("Item 3", 4)}
         self.personaje = pygame.image.load(ruta_imagen).convert_alpha()
-        self.personaje = pygame.transform.scale(self.personaje, (150, h))
-        self.rec_personaje = self.personaje.get_rect(topright=(self.__pos_x, self.__pos_y))
+        self.rec_personaje = self.personaje.get_rect(
+            midbottom=(self.__pos_x, self.__pos_y)
+        )
+        self.gravedad: int = 0
+        self.saltando: bool = False
 
     @abstractmethod
     def izquierda(self):
+        pass
+
+    @abstractmethod
+    def aplicar_gravedad(self):
+        pass
+
+    @abstractmethod
+    def animar_ataque(self):
+        pass
+
+    @abstractmethod
+    def detener_animacion(self):
         pass
 
     @abstractmethod
